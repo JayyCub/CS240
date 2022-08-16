21c8dcc2c78e85d1f6a82427e4d47bbd3c504497
Jacob - ITB <jakethom02@gmail.com>
2022-08-16 15:15:03 -0600

    Evil Hangman Passes Tests

--- EvilHangman/src/hangman/EvilHangman.java
+++ EvilHangman/src/hangman/EvilHangman.java
@@ -1,9 +1,82 @@
 package hangman;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
 public class EvilHangman {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws EmptyDictionaryException, IOException {
+        try {
+            String inputDictionary = args[0];
+            int wordlength = Integer.parseInt(args[1]);
+            int gameCounts = Integer.parseInt(args[2]);
+            if (gameCounts < 1) {
+                throw new ArrayIndexOutOfBoundsException();
+            }
+            String inputGuess;
+            char charGuess;
+            Set<String> possibleWords = new HashSet<>();
+            EvilHangmanGame game = new EvilHangmanGame();
+            File file = new File(inputDictionary);
+            Scanner scanner = new Scanner(System.in);
 
+            game.startGame(file, wordlength);
+
+            while(gameCounts > 0){
+                try {
+                    String word = game.getWord();
+                    promptUser(gameCounts, game.usedLetters, word);
+                    inputGuess = scanner.next();
+                    if (inputGuess.isBlank()){
+                        throw new Exception();
+                    }
+                    charGuess = inputGuess.charAt(0);
+                    if (inputGuess.length() > 1){
+                        throw new Exception();
+                    }
+                    if ((charGuess < 'a' || charGuess > 'z') && (charGuess < 'A' || charGuess > 'Z')){
+                        throw new Exception();
+                    }
+                    possibleWords = game.makeGuess(charGuess);
+
+                    CharSequence dash = "-";
+                    if (!game.getWord().contains(dash)){
+                        gameCounts = -1;
+                    }
+                    if (word.equals(game.getWord())){
+                        gameCounts--;
+                    }
+
+
+                } catch (GuessAlreadyMadeException e){
+                    System.out.println("\nThat guess was already made\n");
+                } catch (Exception e) {
+                    System.out.println("\nInvalid guess\n");
+                }
+            }
+            if (gameCounts < 0){
+                System.out.println("You Win!\nThe word is: ");
+                System.out.println(possibleWords.toArray()[0]);
+            } else {
+                System.out.println("You lose!\n The word is: ");
+                System.out.println(possibleWords.toArray()[0]);
+            }
+
+        } catch (ArrayIndexOutOfBoundsException exception){
+            System.out.print("Number of guesses must be 1 or more");
+        }
     }
 
+    public static void promptUser(int gameCounts, SortedSet<Character> usedLetters, String word){
+        StringBuilder builder = new StringBuilder();
+        builder.append("You have " ).append(gameCounts).append(" guesses left\n").append("Used letters: ");
+        for (Character character: usedLetters){
+            builder.append(character).append(" ");
+        }
+        builder.replace(builder.length() - 1, builder.length(), "\n");
+        builder.append("Word: ").append(word).append("\n").append("Enter guess: ");
+
+        System.out.print(builder);
+    }
 }
--- EvilHangman/src/hangman/EvilHangmanGame.java
+++ EvilHangman/src/hangman/EvilHangmanGame.java
@@ -2,22 +2,116 @@ package hangman;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Set;
-import java.util.SortedSet;
+import java.util.*;
 
 public class EvilHangmanGame implements IEvilHangmanGame{
+    Set<String> wordSet;
+    SortedSet<Character> usedLetters;
+    String word;
+    Map<String, TreeSet<String>> doubleMap;
+
+    public EvilHangmanGame(){
+        wordSet = new HashSet<>();
+        usedLetters = new TreeSet<>();
+        word = "";
+        doubleMap = new HashMap<>();
+    }
+
     @Override
     public void startGame(File dictionary, int wordLength) throws IOException, EmptyDictionaryException {
+        Scanner scanner = new Scanner(dictionary);
+        scanner.useDelimiter("(#[^\\n]*\\n)|(\\s+)+");
+        if (!scanner.hasNext()){
+            throw new EmptyDictionaryException();
+        }
+        wordSet.clear();
+        while (scanner.hasNext()){
+            String string = scanner.next();
+            if (string.length() == wordLength){
+                wordSet.add(string);
+            }
+        }
 
+        if (wordSet.isEmpty()){
+            throw new EmptyDictionaryException();
+        }
+        word = "-".repeat(wordLength);
+        System.out.println(word);
     }
 
     @Override
     public Set<String> makeGuess(char guess) throws GuessAlreadyMadeException {
-        return null;
+        if (guess >= 'A' && guess <= 'Z'){
+            guess += 32;
+        }
+        if (usedLetters.contains(guess)){
+            throw new GuessAlreadyMadeException();
+        }
+        usedLetters.add(guess);
+        String token = findBigSubset(guess);
+        int numHits = updateWordSoFar(guess, token);
+        respondToUser(guess, numHits);
+        return wordSet;
+    }
+
+    private void respondToUser(char guess, int numHits){
+        StringBuilder builder = new StringBuilder();
+        if (numHits > 0){
+            builder.append("Yes, there is ").append(numHits).append(" ").append(guess).append("\n\n");
+        } else {
+            builder.append("Sorry, there are no ").append(guess).append("'s\n\n");
+        } System.out.println(builder);
+    }
+
+    private int updateWordSoFar(char guess, String token){
+        int numHits = 0;
+        for (int i = 0; i < token.length(); i++){
+            if (token.charAt(i) == guess){
+                word = word.substring(0, i) + guess + word.substring(i  + 1);
+                numHits++;
+            }
+        }
+        return numHits;
+    }
+
+    private String findBigSubset(char guess){
+        StringBuilder builder = new StringBuilder();
+        TreeSet<String> temporary;
+        int max = 0;
+        doubleMap.clear();
+
+        for (String string : wordSet){
+            builder.setLength(0);
+            for (int i = 0; i < string.length(); i++){
+                if (string.charAt(i) == guess){
+                    builder.append(guess);
+                } else {
+                    builder.append("-");
+                }
+            }
+            temporary = doubleMap.getOrDefault(builder.toString(), new TreeSet<>());
+            temporary.add(string);
+            doubleMap.put(builder.toString(), temporary);
+            if (temporary.size() > max){
+                max = temporary.size();
+            }
+        }
+        TreeSet<String> sortedPatterns = new TreeSet<>();
+        for (String string : doubleMap.keySet()){
+            if (doubleMap.get(string).size() == max){
+                sortedPatterns.add(string);
+            }
+        }
+        wordSet = doubleMap.get(sortedPatterns.first());
+        return sortedPatterns.first();
     }
 
     @Override
     public SortedSet<Character> getGuessedLetters() {
-        return null;
+        return usedLetters;
+    }
+
+    public String getWord() {
+        return word;
     }
 }
