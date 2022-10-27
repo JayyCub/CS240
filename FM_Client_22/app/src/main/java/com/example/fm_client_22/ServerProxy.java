6f0f0a0ed87238b1ee076debeb87fdb2c15623e7
Jacob T - MacBook <jat0220@byu.edu>
2022-10-26 22:50:09 -0600

    It's Showtime. Client: ServerProxy now connects to server

--- FM_Client_22/app/src/main/java/com/example/fm_client_22/ServerProxy.java
+++ FM_Client_22/app/src/main/java/com/example/fm_client_22/ServerProxy.java
@@ -1,6 +1,15 @@
 package com.example.fm_client_22;
 
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class ServerProxy {
+    String serverHost = "localhost";
+    String serverPort = "8080";
     /*
     login
     register
@@ -15,4 +24,33 @@ public class ServerProxy {
      */
 
     //LoginResult login(LoginRequest request){}
+
+    String connect(){
+        try {
+            URL url = new URL("http://10.0.2.2:8080/clear");
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod("GET");
+            http.setDoOutput(false);
+
+            http.connect();
+            // For some reason the server doesnt actually process the request until some
+            // sort of getResponse... funciton is called. Works regardless, but look into this
+            if (http.getResponseCode() == HttpURLConnection.HTTP_OK){
+                return "Connected, done";
+            }
+
+        } catch (MalformedURLException e) {
+            System.out.println("Errroorrr111");
+            e.printStackTrace();
+            return "Error 1";
+        } catch (IOException e) {
+            System.out.println("errRRRorrr22");
+            e.printStackTrace();
+            return e.toString();
+        } catch (Error error){
+            return "Dif error";
+        }
+        return "Connected";
+    }
+
 }
